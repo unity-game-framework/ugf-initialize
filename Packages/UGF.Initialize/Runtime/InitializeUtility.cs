6b3efb97ec796ff7566d83165e9df2131bc4bf53
Alexander <vorobyovvorobyov@gmail.com>
2019-09-03 21:00:02 +0300

    Add summaries

--- Packages/UGF.Initialize/Runtime/InitializeUtility.cs
+++ Packages/UGF.Initialize/Runtime/InitializeUtility.cs
@@ -7,6 +7,12 @@ namespace UGF.Initialize.Runtime
     /// </summary>
     public static partial class InitializeUtility
     {
+        /// <summary>
+        /// Validates state of the specified handler.
+        /// </summary>
+        /// <param name="expected">The expected initialize state.</param>
+        /// <param name="handler">The initialize state handler to validate.</param>
+        /// <param name="throws">The value that determines whether to throw exception on invalid validate result.</param>
         public static bool ValidateState(bool expected, IInitialize handler, bool throws = true)
         {
             if (handler == null) throw new ArgumentNullException(nameof(handler));
@@ -14,11 +20,24 @@ namespace UGF.Initialize.Runtime
             return ValidateState(expected, handler.IsInitialized, handler.ToString(), throws);
         }
 
+        /// <summary>
+        /// Validates state of the specified handler.
+        /// </summary>
+        /// <param name="expected">The expected initialize state.</param>
+        /// <param name="state">The actual initialize state.</param>
+        /// <param name="throws">The value that determines whether to throw exception on invalid validate result.</param>
         public static bool ValidateState(bool expected, InitializeState state, bool throws = true)
         {
             return ValidateState(expected, state.IsInitialized, state.Name, throws);
         }
 
+        /// <summary>
+        /// Validates state of the specified handler.
+        /// </summary>
+        /// <param name="expected">The expected initialize state.</param>
+        /// <param name="actual">The actual initialize state.</param>
+        /// <param name="name">The name of the state.</param>
+        /// <param name="throws">The value that determines whether to throw exception on invalid validate result.</param>
         public static bool ValidateState(bool expected, bool actual, string name = null, bool throws = true)
         {
             if (string.IsNullOrEmpty(name)) name = "State";
