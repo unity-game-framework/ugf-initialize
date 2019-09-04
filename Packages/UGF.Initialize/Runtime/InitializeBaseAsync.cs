4a3a3504c18a820a8ea4a21a1170fe98720f11fa
Alexander <vorobyovvorobyov@gmail.com>
2019-09-04 21:35:47 +0300

    Add InitializeBaseAsync.IsAsyncInitialized and IsAsyncInProgress

--- Packages/UGF.Initialize/Runtime/InitializeBaseAsync.cs
+++ Packages/UGF.Initialize/Runtime/InitializeBaseAsync.cs
@@ -7,19 +7,24 @@ namespace UGF.Initialize.Runtime
     /// </summary>
     public abstract class InitializeBaseAsync : InitializeBase, IInitializeAsync
     {
+        public bool IsAsyncInitialized { get { return m_initializeAsyncState.IsInitialized && m_initializeAsyncRoutine == null; } }
+        public bool IsAsyncInProgress { get { return m_initializeAsyncRoutine != null; } }
+
         private InitializeState m_initializeAsyncState = new InitializeState();
+        private IEnumerator m_initializeAsyncRoutine;
 
         public IEnumerator InitializeAsync()
         {
             ValidateState(true);
 
             m_initializeAsyncState.Initialize();
+            m_initializeAsyncRoutine = WaitForOnInitializeAsync();
 
-            yield return OnInitializeAsync();
+            return m_initializeAsyncRoutine;
         }
 
         /// <summary>
-        /// Invokes on async initialization.
+        /// Invoked on async initialization.
         /// </summary>
         protected virtual IEnumerator OnInitializeAsync()
         {
@@ -31,6 +36,15 @@ namespace UGF.Initialize.Runtime
             base.OnUninitialize();
 
             m_initializeAsyncState.Uninitialize();
+
+            InitializeUtility.ValidateState(false, IsAsyncInProgress, nameof(IsAsyncInProgress));
+        }
+
+        private IEnumerator WaitForOnInitializeAsync()
+        {
+            yield return OnInitializeAsync();
+
+            m_initializeAsyncRoutine = null;
         }
     }
 }
