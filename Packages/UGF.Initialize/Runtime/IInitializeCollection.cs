44b063b391c66b9807692d81d94484d47dddc966
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-02-19 21:01:03 +0200

    Rework InitializeCollection

---
+++ Packages/UGF.Initialize/Runtime/IInitializeCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UGF.Initialize.Runtime
+{
+    public interface IInitializeCollection : IInitialize, IReadOnlyList<IInitialize>
+    {
+        bool Contains(IInitialize item);
+        void Add(IInitialize item);
+        bool Remove(IInitialize item);
+        void Clear();
+        T Get<T>();
+        IInitialize Get(Type type);
+        bool TryGet<T>(out T item);
+        bool TryGet(Type type, out IInitialize item);
+    }
+}
--- Packages/UGF.Initialize/Runtime/InitializeCollection.cs
+++ Packages/UGF.Initialize/Runtime/InitializeCollection.cs
@@ -1,30 +1,18 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 
 namespace UGF.Initialize.Runtime
 {
-    public class InitializeCollection<TITem> : InitializeBase, IEnumerable<TITem> where TITem : class, IInitialize
+    public class InitializeCollection<TItem> : InitializeBase, IInitializeCollection, IEnumerable<TItem> where TItem : class, IInitialize
     {
-        public IReadOnlyList<TITem> Collection { get; }
+        public int Count { get { return m_collection.Count; } }
+        public TItem this[int index] { get { return m_collection[index]; } }
         public bool ReverseUninitializationOrder { get; set; } = true;
 
-        private readonly List<TITem> m_collection;
+        IInitialize IReadOnlyList<IInitialize>.this[int index] { get { return m_collection[index]; } }
 
-        public InitializeCollection()
-        {
-            m_collection = new List<TITem>();
-
-            Collection = new ReadOnlyCollection<TITem>(m_collection);
-        }
-
-        public InitializeCollection(IEnumerable<TITem> collection)
-        {
-            if (collection == null) throw new ArgumentNullException(nameof(collection));
-
-            m_collection = new List<TITem>(collection);
-        }
+        private readonly List<TItem> m_collection = new List<TItem>();
 
         protected override void OnInitialize()
         {
@@ -56,24 +44,95 @@ namespace UGF.Initialize.Runtime
             }
         }
 
-        public void Add(TITem value)
+        public bool Contains(TItem value)
         {
-            if (value == null) throw new ArgumentNullException(nameof(value));
+            return m_collection.Contains(value);
+        }
 
+        public void Add(TItem value)
+        {
             m_collection.Add(value);
         }
 
-        public bool Remove(TITem value)
+        public bool Remove(TItem value)
         {
             return m_collection.Remove(value);
         }
 
-        public List<TITem>.Enumerator GetEnumerator()
+        public void Clear()
+        {
+            m_collection.Clear();
+        }
+
+        public T Get<T>()
+        {
+            return (T)Get(typeof(T));
+        }
+
+        public IInitialize Get(Type type)
+        {
+            if (!TryGet(type, out IInitialize value))
+            {
+                throw new ArgumentException($"Value by the specified type not found: '{type}'.");
+            }
+
+            return value;
+        }
+
+        public bool TryGet<T>(out T item)
+        {
+            if (TryGet(typeof(T), out IInitialize value))
+            {
+                item = (T)value;
+                return true;
+            }
+
+            item = default;
+            return false;
+        }
+
+        public bool TryGet(Type type, out IInitialize item)
+        {
+            for (int i = 0; i < m_collection.Count; i++)
+            {
+                item = m_collection[i];
+
+                if (type.IsInstanceOfType(item))
+                {
+                    return true;
+                }
+            }
+
+            item = null;
+            return false;
+        }
+
+        public List<TItem>.Enumerator GetEnumerator()
         {
             return m_collection.GetEnumerator();
         }
 
-        IEnumerator<TITem> IEnumerable<TITem>.GetEnumerator()
+        bool IInitializeCollection.Contains(IInitialize item)
+        {
+            return Contains((TItem)item);
+        }
+
+        void IInitializeCollection.Add(IInitialize item)
+        {
+            Add((TItem)item);
+        }
+
+        bool IInitializeCollection.Remove(IInitialize item)
+        {
+            return Remove((TItem)item);
+        }
+
+        IEnumerator<TItem> IEnumerable<TItem>.GetEnumerator()
+        {
+            return m_collection.GetEnumerator();
+        }
+
+        IEnumerator<IInitialize> IEnumerable<IInitialize>.GetEnumerator()
         {
             return m_collection.GetEnumerator();
         }
