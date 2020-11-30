92fcb82f69eba109e670198020db5e69deac0bb5
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-30 21:31:16 +0300

    Update InitializeCollection.cs

--- Packages/UGF.Initialize/Runtime/InitializeCollection.cs
+++ Packages/UGF.Initialize/Runtime/InitializeCollection.cs
@@ -46,16 +46,22 @@ namespace UGF.Initialize.Runtime
 
         public bool Contains(TItem value)
         {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
             return m_collection.Contains(value);
         }
 
         public void Add(TItem value)
         {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
             m_collection.Add(value);
         }
 
         public bool Remove(TItem value)
         {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
             return m_collection.Remove(value);
         }
 
@@ -88,6 +94,8 @@ namespace UGF.Initialize.Runtime
 
         public bool TryGet(Type type, out IInitialize item)
         {
+            if (type == null) throw new ArgumentNullException(nameof(type));
+
             for (int i = 0; i < m_collection.Count; i++)
             {
                 item = m_collection[i];
