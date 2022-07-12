e4af3102178aa965bfabb298b0132ab3659233ec
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-12 14:59:33 +0300

    Add InitializableAsync

---
+++ Packages/UGF.Initialize/Runtime/InitializableAsync.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace UGF.Initialize.Runtime
+{
+    public class InitializableAsync : Initializable, IInitializeAsync
+    {
+        public bool IsInitializedAsync { get { return m_state; } }
+
+        private InitializeState m_state;
+
+        protected virtual Task OnInitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override void OnUninitialize()
+        {
+            base.OnUninitialize();
+
+            if (m_state)
+            {
+                m_state = m_state.Uninitialize();
+            }
+        }
+
+        public Task InitializeAsync()
+        {
+            m_state = m_state.Initialize();
+
+            return OnInitializeAsync();
+        }
+    }
+}
